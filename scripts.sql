7216fed6946c96e2b581558460a027e5c1d45043
Msingathi.Majola <Msingathi.Majola@investec.co.za>
2021-03-10 16:23:42 +0200

    initial commit

---
+++ scripts.sql
@@ -0,0 +1,9 @@
+Select * from ov_exception_report
+where incoming_ov_entity_id = '759b8fe2-763f-4a1b-8686-1554da517256';
+
+
+Select * from ov_exception_report
+where incoming_ov_entity_id = '5ccede60-235f-4e38-be81-6498fe9cd2d4';
+
+Select * from ov_exception_report
+where incoming_ov_entity_id = 'd8b1f8e3-79f7-4cf2-b137-e53f4dc72117';
